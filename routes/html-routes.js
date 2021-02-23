75415e3569449b08d7c3722d06c0df78b6f0694c
Imari Pickens <imaridp23@gmail.com>
2021-02-22 20:27:59 -0500

    Adding clarifying comments

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,16 @@
+// Setting Dependencies
 const router = require("express").Router();
 const path = require("path");
 
+// Method to Display Exercise Page
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+// Method to Display Stats Page
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
+// Exporting to server.js file
 module.exports = router;
