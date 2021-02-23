e441a2b3585e702d66c798bb8fe8a7a7f4e7f710
Imari Pickens <imaridp23@gmail.com>
2021-02-22 20:23:33 -0500

    Adding clarifying comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,8 @@
+// Setting Dependencies
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+// Get Route to Display All Workouts
 router.get("/api/workouts/all", (req, res) => {
   Workout.find({}).then(dbWorkouts => {
       res.json(dbWorkouts);
@@ -9,6 +11,7 @@ router.get("/api/workouts/all", (req, res) => {
     });
 });
 
+// Get Route to Retrieve a Range of Workouts
 router.get("/api/workouts/range", ({ query }, res) => {
   Workout.find({ day: {$gte: new Date(new Date().setDate(new Date().getDate()-7)), $lte: new Date()}})
     .then(dbWorkouts => {
@@ -18,6 +21,7 @@ router.get("/api/workouts/range", ({ query }, res) => {
     });
 });
 
+// Post Route to Enter New Exercise into Database
 router.post("/api/workouts", (req, res) => {
   Workout.create({}).then(dbWorkout => {
       res.json(dbWorkout);
@@ -26,6 +30,7 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
+// Put Route to Update Exercise
 router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
@@ -38,6 +43,7 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
     });
 });
 
+// Delete Route to Remove an Exercise from the Database 
 router.delete("/api/workouts", ({ body }, res) => {
   Workout.findByIdAndDelete(body.id)
     .then(() => {
@@ -47,4 +53,5 @@ router.delete("/api/workouts", ({ body }, res) => {
     });
 });
 
+// Exporting Routes to server.js file
 module.exports = router;
