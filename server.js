fbde05ebb2b9c17c618ccd9b7f50b8e163fe72dd
Imari Pickens <imaridp23@gmail.com>
2021-02-22 19:01:34 -0500

    Adding server.js file

---
+++ server.js
@@ -0,0 +1,31 @@
+// Setting Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Setting Port
+const PORT = process.env.PORT || 3000;
+
+// Creating Express Server
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Creating connection for database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+// Routes for API and HTML pages
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+// Event listener for starting server
+app.listen(PORT, () => {
+  console.log(`Your app is running on Port ${PORT}!`);
+});
